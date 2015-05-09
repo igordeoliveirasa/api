84023793fed141d90d3bc6b370b0fcfa05e7a6c4
Igor de Oliveira Sa <igordeoliveirasa@gmail.com>
2015-05-09 19:01:05 -0300

    kickoff

---
+++ api.js
@@ -0,0 +1,10 @@
+/**
+ * Created by igor on 09/05/15.
+ */
+
+
+var Api = function(data) {
+    this.print = function(){
+        console.log(data);
+    }
+}
---
+++ specs/api.spec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by igor on 09/05/15.
+ */
+
+
+
+describe("Api tests", function() {
+    it("contains spec with an expectation", function() {
+        var api = new Api("oi porra");
+        api.print();
+    });
+});
